1c2e08a2f14d3d5a13660930a47dc8c681e76d9a
Jasper <80272820+Code-Jap@users.noreply.github.com>
2021-05-06 17:18:07 +0200

    Add files via upload

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,4 +1,33 @@
-let number = 3 + 5 * 2 / 2;
-let surName = 'jap';
-let age = "drieendertig";
-console.log(number, surName, age);
+console.log("Hello Winc Academy")
+
+let name = 'Jasper';
+console.log(name);
+
+let math = 7 + 5;
+console.log(math);
+
+let mathString = "3" + "4";
+console.log(mathString)
+
+let addition = 76 + 77;
+console.log(addition);
+
+let subtraction = 99 - 34;
+console.log(subtraction);
+
+let multiplication = 66 * 5;
+console.log(multiplication)
+
+let division = 99 / 3;
+console.log(division)
+
+let modulus = 50 % 200;
+console.log(modulus)
+
+let age = 33;
+console.log(typeof age);
+
+let otherAge = '33';
+console.log(typeof otherAge)
+
+ 
